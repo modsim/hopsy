43fa435dbd26f7d15d599722f8cf4c8848ee185b
Richard D. Paul <richard@los-paul.eu>
2021-04-20 15:33:05 +0200

    rename to hopsy

--- setup.py
+++ setup.py
@@ -102,13 +102,13 @@ class CMakeBuild(build_ext):
 # The information here can also be placed in setup.cfg - better separation of
 # logic and declaration, and simpler if you include description/version in a file.
 setup(
-    name="cmake_example",
+    name="hopsy",
     version="0.0.1",
-    author="Dean Moldovan",
-    author_email="dean0x7d@gmail.com",
-    description="A test project using pybind11 and CMake",
+    author="Richard D. Paul",
+    author_email="r.paul@fz-juelich.de",
+    description="a python interface of hops, the highly optimized polytope sampling toolbox",
     long_description="",
-    ext_modules=[CMakeExtension("cmake_example")],
+    ext_modules=[CMakeExtension("hopsy")],
     cmdclass={"build_ext": CMakeBuild},
     zip_safe=False,
 )
