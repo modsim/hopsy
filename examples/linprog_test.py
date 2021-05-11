fbbcb1835d2e9fbdfcbde6b8d62c763bfed4a34b
Fredrik Jadebeck <johann.fredrik@jadebeck.dev>
2021-05-11 12:46:17 +0200

    fix double free

--- examples/linprog_test.py
+++ examples/linprog_test.py
@@ -1,19 +1,21 @@
 import hopsy
 import numpy as np
 
-A = np.array([[1, 1], [-1, 0], [0, -1]])
-b = np.array([[1], [0], [0]]);
+if __name__ == "__main__":
+    A = np.array([[1, 1], [-1, 0], [0, -1]])
+    b = np.array([[1], [0], [0]]);
 
-mu = np.zeros((2,1))
-cov = np.identity(2)
+    mu = np.zeros((2,1))
+    cov = np.identity(2)
 
-model = hopsy.MultivariateGaussianModel(mu, cov)
-problem = hopsy.Problem(A, b, model)
-run = hopsy.Run(problem)
+    model = hopsy.MultivariateGaussianModel(mu, cov)
+    problem = hopsy.Problem(A, b, model)
+    run = hopsy.Run(problem)
 
-# this computes the chebyshev center as no other starting point has been
-# previously set
-run.init()
+    # this computes the chebyshev center as no other starting point has been
+    # previously set
+    run.init()
 
-run.sample()
+    result = run.sample()
+    print(result)
 
