f7581fe49cc99127cbb0e8df61a535b6497b62a3
Richard D. Paul <richard@los-paul.eu>
2021-05-12 13:18:05 +0200

    remove container if it is there before

--- makewheels.sh
+++ makewheels.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker ps -a | grep hopsy_manylinux_build_env && (docker stop hopsy_manylinux_build_env; docker rm hopsy_manylinux_build_env)
+
 docker run -d -t --name hopsy_manylinux_build_env -v $(pwd):/io quay.io/pypa/manylinux2014_x86_64
 
 for py in "cp310-cp310" "cp36-cp36m" "cp37-cp37m" "cp38-cp38" "cp39-cp39";
