ac508ca35148756729b4571ec7c57dcc6569c12e
Richard D. Paul <richard@los-paul.eu>
2021-09-13 12:56:54 +0200

    first tests for data loading and utility plot functions

---
+++ examples/data_test.py
@@ -0,0 +1,43 @@
+import hopsy as hp
+import numpy as np
+import hopsy.util as util
+import matplotlib.pyplot as plt
+
+from copy import deepcopy
+
+import os
+import sys
+
+data = hp.Data()
+data.states = [[np.array([0, 0])], [np.array([0, 0])]]
+
+model = hp.MultivariateGaussianModel()
+problem = hp.Problem([[1, 0]], [5], model)
+problem = hp.add_box_constraints(problem, -5, 5)
+
+n_chains = 2
+
+x0 = [0.2, 0.6]
+starting_points = [deepcopy(x0) for x in range(n_chains)]
+
+run = hp.Run(problem, number_of_chains = n_chains, starting_points=starting_points)
+
+run.sample(1000, 10)
+print(np.array(run.data.states).shape)
+
+run.data.write("test_output", False)
+
+if len(sys.argv) == 1 or sys.argv[1] != "test":
+    fig, axs = util.jointplot(run.data, density_estimator=util.gaussian_mixture)
+    plt.show()
+
+data = util.load("test_output")
+
+print(data.states)
+
+if len(sys.argv) == 1 or sys.argv[1] != "test":
+    print(np.array(data.states).shape)
+    fig, axs = util.jointplot(data, density_estimator=util.gaussian_mixture)
+    plt.show()
+
+os.rmdir("test_output")
