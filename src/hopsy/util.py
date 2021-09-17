ae1ad1553d7c6151695ec1fcc4990e13cab6647d
Richard D. Paul <richard@los-paul.eu>
2021-09-17 16:53:11 +0200

    update

--- src/hopsy/util.py
+++ src/hopsy/util.py
@@ -123,11 +123,15 @@ def scatterplot(data,
 
     # hack to save an if statement since int(True) == 1 and int(False) == 0
     colors = [cm.tab10(int((distinguish_chains) * i) % 10) for i in range(number_of_chains)]
+    alpha = np.max([1./len(states[0]), 1./255]) # apperantly minimal valid alpha
+
+    if "alpha" not in kwargs:
+        kwargs["alpha"] = alpha
 
     for i in range(rows):
         for c in range(number_of_chains):
-            for j in range(1, cols-i):
-                axs[i][j].scatter(states[c,:,i], states[c,:,j+i], color=colors[c], alpha = 1./number_of_chains, **kwargs)
+            for j in range(cols-i):
+                axs[i][j].scatter(states[c,:,j+i+1], states[c,:,i], color=colors[c], **kwargs)
 
     return fig, axs
 
@@ -158,8 +162,8 @@ def densityplot(data,
 
     for i in range(rows):
         for c in range(number_of_chains):
-            for j in range(1, cols-i):
-                xx, yy, zz = density_estimator(states[c,:,i], states[c,:,j+i], **kwargs)
+            for j in range(cols-i):
+                xx, yy, zz = density_estimator(states[c,:,i], states[c,:,j+i+1], **kwargs)
                 axs[i][j].contour(xx, yy, zz, alpha = 1./number_of_chains, **kwargs)
 
     return fig, axs
@@ -177,12 +181,44 @@ def scatterdensityplot(data,
         return fig, axs
 
 
+def histplot(data,
+              fig = None,
+              axs = None,
+              distinguish_chains = True,
+              dims = None,
+              **kwargs):
+    states = np.array(data.states)
+    dim = len(states[0,0]) if dims is None else len(dims)
+    dims = range(dim) if dims is None else dims
+    number_of_chains = len(states)
+
+    if fig is None and axs is None:
+        fig = plt.figure()
+
+    if axs is None:
+        rows, cols = int(np.ceil(np.sqrt(dim))), int(np.ceil(np.sqrt(dim)))
+        axs = [[fig.add_subplot(rows, cols, i*cols + j + 1) for j in range(cols)] for i in range(rows)]
+    else:
+        rows, cols = len(axs), len(axs[0])
+
+    # hack to save an if statement since int(True) == 1 and int(False) == 0
+    colors = [cm.tab10(int((distinguish_chains) * i) % 10) for i in range(number_of_chains)]
+
+    for i in range(rows):
+        for j in range(cols):
+            if j < len(axs[i]):
+                for c in range(number_of_chains):
+                    axs[i][j].hist(states[c,:,dims[i*cols+j]], color=colors[c], alpha = 1./number_of_chains)
+
+    return fig, axs
+
 def jointplot(data,
               fig = None,
               axs = None,
               distinguish_chains = True,
               dims = None,
-              off_diag_plot=scatterdensityplot,
+              diag_plot=histplot,
+              off_diag_plot=scatterplot,
               **kwargs):
     states = np.array(data.states)
     dim = len(states[0,0]) if dims is None else len(dims)
@@ -193,21 +229,19 @@ def jointplot(data,
 
     if axs is None:
         rows, cols = dim, dim
-        print(rows, cols)
         # only populate upper triangular part with subplots
         axs = [[fig.add_subplot(rows, cols, (i*rows)+j+1) for j in range(i, cols)] for i in range(rows)]
 
-    # hack to save an if statement since int(True) == 1 and int(False) == 0
-    colors = [cm.tab10(int((distinguish_chains) * i) % 10) for i in range(number_of_chains)]
-
+    diag_axs = []
     off_diag_axs = []
     for i in range(rows):
         off_diag_axs.append([])
-        for c in range(number_of_chains):
-            axs[i][0].hist(states[c,:,i], color=colors[c], alpha = 1./number_of_chains)
-            for j in range(1, cols-i):
-                off_diag_axs[i].append(axs[i][j])
+        diag_axs.append(axs[i][0])
 
+        for j in range(1, cols-i):
+            off_diag_axs[i].append(axs[i][j])
+
+    diag_plot(data, fig, [diag_axs], distinguish_chains, dims, **kwargs)
     off_diag_plot(data, fig, off_diag_axs, distinguish_chains, dims, **kwargs)
 
     return fig, axs
