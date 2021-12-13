89d826cfa6ce7a382d66f34575223d370299a38b
Richard D. Paul <richard@los-paul.eu>
2021-12-13 18:01:01 +0100

    add first tuning tests

--- src/hopsy/tests/tuning.py
+++ src/hopsy/tests/tuning.py
@@ -3,5 +3,30 @@ import unittest
 from .. import *
 
 class TuningTests(unittest.TestCase):
-    pass
+
+    def test_tuning_call(self):
+        problem = hopsy.Problem([[1, 1]], [1], hopsy.Gaussian(), starting_point=[0, 0]); 
+        mcs = [hopsy.MarkovChain(hopsy.GaussianProposal(problem), problem) for i in range(5)]; 
+
+        ts = hopsy.ThompsonSamplingTuning()
+        target = hopsy.ExpectedSquaredJumpDistanceTarget(mcs)
+
+        # run twice to check that tune() doesnt break anything in target
+        hopsy.tune(ts, target, [hopsy.RandomNumberGenerator(0, i) for i in range(5)])
+        hopsy.tune(ts, target, [hopsy.RandomNumberGenerator(0, i) for i in range(5)])
+
+        self.assertEqual(True, True)
+
+
+    def test_target_setup(self):
+        problem = hopsy.Problem([[1, 1]], [1], hopsy.Gaussian(), starting_point=[0, 0]); 
+        mcs = [hopsy.MarkovChain(hopsy.GaussianProposal(problem), problem) for i in range(5)]; 
+
+        ts = hopsy.ThompsonSamplingTuning()
+        target = hopsy.ExpectedSquaredJumpDistanceTarget(markov_chains=mcs)
+        hopsy.tune(ts, target, [hopsy.RandomNumberGenerator(0, i) for i in range(5)])
+        hopsy.tune(ts, target, [hopsy.RandomNumberGenerator(0, i) for i in range(5)])
+
+        self.assertListEqual(target([0], [hopsy.RandomNumberGenerator(0, i) for i in range(5)]), (0.8263665206373976, 0.005821290885441588))
+
 
