a6c2ebe07b6a41c37362d1c4da22e31c4b0df855
Richard D. Paul <richard@los-paul.eu>
2022-01-30 15:39:18 +0100

    fix doc str badge

--- .makedocstrbadge.py
+++ .makedocstrbadge.py
@@ -1,12 +1,12 @@
 import anybadge
 
 with open("docs/.docstrcovreport.txt", "r") as f:
-    docstr_coverage = f.readlines()[-1].split(": ")[1].strip('\n').strip('%')
+    docstr_coverage = int(float(f.readlines()[-1].split(": ")[1].strip('\n').strip('%')))
     thresholds = {50: 'red',
                   60: 'orange',
                   75: 'yellow',
                   90: 'green'}
 
-    badge = anybadge.Badge('docstr cov', str(docstr_coverage) + "%", thresholds=thresholds)
+    badge = anybadge.Badge('docstr cov', docstr_coverage, value_suffix="%", thresholds=thresholds)
     badge.write_badge('docs/docstrcov.svg', overwrite=True)
 
