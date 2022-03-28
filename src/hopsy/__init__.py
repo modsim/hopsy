b2f3a6c39579ad2bd9680da3fbfb02a184643c53
Richard D. Paul <richard@los-paul.eu>
2022-03-28 11:13:17 +0200

    rename hopsy.__debug__ to hopsy.__is_debug__

--- src/hopsy/__init__.py
+++ src/hopsy/__init__.py
@@ -1,4 +1,4 @@
 from . core import *
 del globals()['MarkovChain']
-from . core import __version__, __build__, __debug__
+from . core import __version__, __build__, __is_debug__
 from . misc import *
