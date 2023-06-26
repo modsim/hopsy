f8f886e984f6829330cd531828619d897974d8a1
Jadebeck, Johann Fredrik <j.jadebeck@fz-juelich.de>
2023-06-26 17:59:49 +0200

    Resolve "Bump pybind"

--- makewheels.sh
+++ makewheels.sh
@@ -6,7 +6,7 @@ docker ps -a | grep hopsy_manylinux_build_env && (docker stop hopsy_manylinux_bu
 
 docker run -d -t --name hopsy_manylinux_build_env -v $(pwd):/io quay.io/pypa/manylinux2014_x86_64
 
-for py in "cp36-cp36m" "cp37-cp37m" "cp38-cp38" "cp39-cp39" "cp310-cp310";
+for py in "cp36-cp36m" "cp37-cp37m" "cp38-cp38" "cp39-cp39" "cp310-cp310" "cp311-cp311";
 #for py in "cp38-cp38";
 do
 #    docker exec hopsy_manylinux_build_env /bin/bash -c  \
