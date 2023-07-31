a0ef4b415e378b2ac4b289dd0800f0279730b57b
CI_BOT <j.jadebeck@fz-juelich.de>
2023-07-31 22:51:41 +0000

    Merge branch '78-update-manifest-in-to-create-compilable-sdist-distributions-of-hopsy' into 'develop'
    Resolve "Update MANIFEST.in to create compilable sdist distributions of hopsy"
    
    Closes #78
    
    See merge request IBG-1/ModSim/hopsy!48 + docs

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.2.1b0',
+    VERSION: '1.3.0b4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
