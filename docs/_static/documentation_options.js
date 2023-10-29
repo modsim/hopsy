2ba33c82a6a285c4f606e5cf694d6795d4db5406
CI_BOT <j.jadebeck@fz-juelich.de>
2023-10-29 17:14:19 +0000

    Resolve "Add helper function to check whether polytope is empty or not"
    Closes #101
    
    See merge request IBG-1/ModSim/hopsy!64 + docs

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.4.0',
+    VERSION: '1.4.0b0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
@@ -7,7 +7,7 @@ const DOCUMENTATION_OPTIONS = {
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: true,
+    NAVIGATION_WITH_KEYS: false,
     SHOW_SEARCH_SUMMARY: true,
     ENABLE_SEARCH_SHORTCUTS: true,
 };
