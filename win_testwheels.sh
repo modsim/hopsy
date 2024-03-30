d90048fcfe094b93664ecf1ebf33fd70965ded7b
Jadebeck, Johann Fredrik <j.jadebeck@fz-juelich.de>
2024-03-30 16:14:00 +0100

    Add support for python3.12

--- win_testwheels.sh
+++ win_testwheels.sh
@@ -1,3 +1,19 @@
-
-
-conda activate py3.7 && python --version && python -c "import hopsy; print(hopsy.__version__, hopsy.__build__)" && conda activate py3.8 && python --version && python -c "import hopsy; print(hopsy.__version__, hopsy.__build__)" && conda activate py3.9 && python --version && python -c "import hopsy; print(hopsy.__version__, hopsy.__build__)" && conda activate py3.10 && python --version && python -c "import hopsy; print(hopsy.__version__, hopsy.__build__)" && conda activate py3.11 && python --version && python -c "import hopsy; print(hopsy.__version__, hopsy.__build__)"
+conda activate py3.8
+python --version
+python -c "import hopsy; print(hopsy.__version__, hopsy.__build__)"
+python -m unittest tests
+conda activate py3.9
+python --version
+python -c "import hopsy; print(hopsy.__version__, hopsy.__build__)"
+conda activate py3.10
+python --version
+python -c "import hopsy; print(hopsy.__version__, hopsy.__build__)"
+python -m unittest tests
+conda activate py3.11
+python --version
+python -c "import hopsy; print(hopsy.__version__, hopsy.__build__)"
+python -m unittest tests
+conda activate py3.12
+python --version
+python -c "import hopsy; print(hopsy.__version__, hopsy.__build__)"
+python -m unittest tests
