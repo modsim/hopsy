18b7e6a649e68d5998a170e5e8c7cc3d81a68e02
Richard D. Paul <r.paul@fz-juelich.de>
2024-04-09 17:59:23 +0200

    fix imports in tuning.py

--- tests/tuning.py
+++ tests/tuning.py
@@ -34,7 +34,7 @@ class TuningTests(unittest.TestCase):
         self.assertLessEqual(result[1], 0.05)
         self.assertGreaterEqual(result[1], 0.005)
 
-        tune(ts, target, [RandomNumberGenerator(0, i) for i in range(5)])
+        tune_old(ts, target, [RandomNumberGenerator(0, i) for i in range(5)])
 
     def test_accrate_target_setup(self):
         problem = Problem([[1, 1]], [1], Gaussian(), starting_point=[0, 0])
@@ -42,7 +42,7 @@ class TuningTests(unittest.TestCase):
 
         ts = ThompsonSamplingTuning()
         target = AcceptanceRateTarget(mcs, acceptance_rate=0.825)
-        stepsize, posterior = tune(
+        stepsize, posterior = tune_old(
             ts, target, [RandomNumberGenerator(0, i) for i in range(5)]
         )
 
