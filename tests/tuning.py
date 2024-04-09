612ebe6298fe62a5e295884c46a0687959ed3deb
Richard D. Paul <r.paul@fz-juelich.de>
2024-04-09 15:42:48 +0200

    fixed imports

--- tests/tuning.py
+++ tests/tuning.py
@@ -56,40 +56,40 @@ class TuningTests(unittest.TestCase):
 
     def test_python_tuning(self):
         for target in ["accrate", "esjd", "esjd/s"]:
-            A, b = hopsy.examples.generate_unit_hypercube(10)
-            problem = hopsy.Problem(
-                A, b, hopsy.Gaussian(mean=np.zeros(10), covariance=0.001 * np.eye(10))
+            A, b = examples.generate_unit_hypercube(10)
+            problem = Problem(
+                A, b, Gaussian(mean=np.zeros(10), covariance=0.001 * np.eye(10))
             )
-            problem.starting_point = hopsy.compute_chebyshev_center(problem)
+            problem.starting_point = compute_chebyshev_center(problem)
 
             proposals = [
-                hopsy.GaussianProposal,
-                hopsy.GaussianHitAndRunProposal,
-                hopsy.BallWalkProposal,
-                hopsy.CSmMALAProposal,
-                hopsy.DikinWalkProposal,
+                GaussianProposal,
+                GaussianHitAndRunProposal,
+                BallWalkProposal,
+                CSmMALAProposal,
+                DikinWalkProposal,
             ]
 
             mcs = [
-                [hopsy.MarkovChain(problem, proposal=proposal) for i in range(4)]
+                [MarkovChain(problem, proposal=proposal) for i in range(4)]
                 for proposal in proposals
             ]
             rngs = [
-                [hopsy.RandomNumberGenerator(42, 4 * j + i) for i in range(4)]
+                [RandomNumberGenerator(42, 4 * j + i) for i in range(4)]
                 for j, _ in enumerate(proposals)
             ]
 
             _, _, (gprs, domains) = tune(mcs, rngs, target=target, n_tuning=10000)
 
-            proposal = hopsy.GaussianProposal
+            proposal = GaussianProposal
 
-            mcs = [hopsy.MarkovChain(problem, proposal=proposal) for i in range(4)]
-            rngs = [hopsy.RandomNumberGenerator(42, i) for i in range(4)]
+            mcs = [MarkovChain(problem, proposal=proposal) for i in range(4)]
+            rngs = [RandomNumberGenerator(42, i) for i in range(4)]
 
             _, _, (gprs, domains) = tune(mcs, rngs, target=target, n_tuning=10000)
 
-            mcs = hopsy.MarkovChain(problem, proposal=proposal)
-            rngs = hopsy.RandomNumberGenerator(42)
+            mcs = MarkovChain(problem, proposal=proposal)
+            rngs = RandomNumberGenerator(42)
 
             _, _, (gprs, domains) = tune(mcs, rngs, target=target, n_tuning=10000)
 
