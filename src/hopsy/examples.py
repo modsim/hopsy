d34c1458c52474f6a08d5f7f2505d937cc7a4991
onikolskyy <onikolskyy@gmail.com>
2024-10-31 14:13:01 +0100

    GaussianMixtureToyProblemGenerator

--- src/hopsy/examples.py
+++ src/hopsy/examples.py
@@ -18,6 +18,8 @@ class _submodules:
     import scipy
 
 
+
+
 _s = _submodules
 
 
@@ -133,3 +135,205 @@ class BirkhoffPolytope:
         """
 
         return self.kernel_basis @ samples.T + self.particular_sol.reshape((-1, 1))
+
+
+
+
+
+def to_angle(gamma):
+    return gamma * 90
+
+
+def to_rad(gamma):
+    return (0.25 + .25 * gamma) * _s.np.pi
+
+
+
+def spike(dim, gamma):
+    angle = to_rad(gamma)
+    x, y = _s.np.cos(angle), _s.np.sin(angle)
+    A = _s.np.zeros((int(dim*(dim-1)), dim))
+    b = _s.np.zeros(int(dim*(dim-1)))
+    
+    l = _s.np.sqrt(x**2 + y**2)
+    
+    k = 0
+    for i in range(dim):
+        for j in range(1, dim):
+            A[k,i] = x / l
+            A[k,(i+j) % dim] = -y / l
+            
+            k += 1
+            
+    prob = _c.Problem(A, b)
+    prob = _c.add_box_constraints(prob, 0, 1)
+    return prob.A, prob.b
+
+
+
+
+
+
+class GaussianMixtureToyProblemGenerator:
+    """
+    A generator for toy problems using Gaussian Mixture Models.
+
+    Parameters
+    ----------
+    n_modes : int, optional
+        Number of Gaussian modes in the mixture. Default is None.
+    mode_locs : list, optional
+        A list of locations for each Gaussian mode in the mixture. Default is an empty list.
+    scales : list, optional
+        A list of scale values (e.g., standard deviations) for each Gaussian mode in the mixture. Default is an empty list.
+    A : array-like, optional
+        A transformation matrix applied to the Gaussian mixture. Default is None.
+    b : array-like, optional
+        A translation vector applied to the Gaussian mixture. Default is None.
+    dim : int, optional
+        The dimensionality of the Gaussian modes. Default is None.
+    n_nonident : int, optional
+        Number of non-identifiable parameters in the problem. Default is 0.
+    seed : int, optional
+        A seed for random number generation to ensure reproducibility. Default is None.
+
+    Attributes
+    ----------
+    n_modes : int
+        Number of Gaussian modes in the mixture.
+    mode_locs : list
+        Locations of each Gaussian mode.
+    scales : list
+        Scale values for each Gaussian mode.
+    A : array-like
+        Transformation matrix.
+    b : array-like
+        Translation vector.
+    dim : int
+        Dimensionality of the Gaussian modes.
+    n_nonident : int
+        Number of non-identifiable parameters.
+    seed : int
+        Random seed for reproducibility.
+
+    """
+
+    def __init__(
+        self, n_modes=None, 
+        mode_locs=None, scales=None, 
+        polytope_type=None, angle=None,
+        A=None, b=None, dim=None, 
+        n_nonident=0, seed=None
+    ):
+        self.n_modes = n_modes
+        self.mode_locs = mode_locs if mode_locs is not None else []
+        self.scales = scales if scales is not None else []
+        self.A = A
+        self.b = b
+        self.dim = dim
+        self.n_nonident = n_nonident
+        self.seed = seed
+
+        if self.seed is not None:
+            _s.np.random.seed(self.seed)
+
+        if self.n_modes is None and len(self.scales) == 0:
+            raise ValueError("Either n_modes or scales must be provided")
+        
+        if self.n_modes is not None and len(self.scales) > 0:
+            raise ValueError("Only one of n_modes or scales can be provided")
+        
+
+        if self.dim is None:
+            if self.A is None:
+                raise ValueError("A must be provided if dim is not provided")
+            if self.b is None:
+                raise ValueError("b must be provided if dim is not provided")
+            if self.A.shape[0] != self.b.shape[0]:
+                raise ValueError("A and b must have the same number of rows")
+        else:
+            if self.A is not None or self.b is not None:
+                raise ValueError("If dim is provided, A and b must not be provided")
+
+        if self.A is None and self.b is not None:
+            raise ValueError("A and b must be provided together")
+
+        if (self.A is not None) and (self.b is None):
+            raise ValueError("A and b must be provided together")
+
+        if self.A is not None and self.polytope_type is not None:
+            raise ValueError("A and polytope_type must not be provided together")
+        
+        self.polytope_type = polytope_type
+
+        if self.polytope_type is not None:
+            if self.polytope_type == "spike":
+                if angle is None:
+                    raise ValueError("angle must be provided for spike polytope")
+                self.A, self.b = spike(self.dim, angle)
+            else :
+                raise ValueError("Unknown polytope type")
+            
+
+        if self.dim is None:
+            self.dim = self.A.shape[1]
+
+        if self.scales is not None:
+            self.scales = [
+                self.generate_covariance_mat((1e-2, 1e6)) for i in range(self.n_modes)
+            ]
+
+        if len(self.mode_locs) == 0:
+            self.mode_locs = [_s.np.random.rand(self.dim) for i in range(self.n_modes)]
+
+        if self.A is None and self.b is None:
+            self.A, self.b = _c.generate_unit_hypercube(self.dim)
+
+        
+
+
+    """
+    Genrate a random covariance matrix with n_nonident non-identifiable directions
+    """
+    def generate_covariance_mat(
+            self,
+            scales_range : _s.typing.Tuple[float, float] = (1, 1)
+    ) -> _s.np.ndarray:
+        
+        a = _s.np.random.rand(self.dim, self.dim)
+        q,_ = _s.np.linalg.qr(a)
+        # q is othonormal
+        idx_nonident = _s.np.random.choice(self.dim, self.n_nonident, replace=False)
+        # print(idx_nonident)
+        eig = _s.np.identity(self.dim)
+        scales = _s.np.array([scales_range[0]]*self.dim)
+        scales[idx_nonident] = scales_range[1]
+
+        eig = _s.np.diag(scales) @ eig
+        cov = q @ eig @ q.T
+
+        return cov
+
+        
+
+    def generate_problem(self) -> _c.Problem:
+
+        models = [
+            _c.Gaussian(mean, covariance)
+            for mean, covariance in zip(self.mode_locs, self.scales)
+        ]
+
+        mixture = _c.Mixture(models)
+
+        return _c.Problem(
+            A=self.A,
+            b=self.b,
+            model=mixture
+        )
+
+
+    def get_gassian_mixture(self):
+        return [
+                _s.scipy.stats.multivariate_normal(mean, covariance)
+                for mean, covariance in zip(self.mode_locs, self.scales)
+            ]
