d7a6c5e32ef68e3a895c9613c2a7eb26b8b75b60
Anton Stratmann <a.stratmann@fz-juelich.de>
2025-08-13 09:29:13 +0200

    Improve tests

--- tests/annealing.py
+++ tests/annealing.py
@@ -1,6 +1,7 @@
 import unittest
 
 import numpy as np
+import scipy
 
 from hopsy import *
 
@@ -10,7 +11,7 @@ thinning = 10
 
 
 class AnnealingTests(unittest.TestCase):
-    def test_no_error_on_simple_execution(self):
+    def test_no_error_on_execution(self):
         problem = Problem(
             np.array([[1], [-1]]), np.array([3, 3]), Gaussian([0.0], [[1.0]])
         )
@@ -25,3 +26,22 @@ class AnnealingTests(unittest.TestCase):
             progress_bar=True,
         )
         self.assertTrue(True)
+
+    def test_similar_wasserstein_to_single_chain(self):
+        problem = Problem(
+            np.array([[1], [-1]]), np.array([3, 3]), Gaussian([0.0], [[1.0]])
+        )
+        samples_nrpt = sample_pt(
+            5,
+            8,
+            problem,
+            compute_chebyshev_center(problem),
+            GaussianHitAndRunProposal,
+            42,
+            proposal_args={"stepsize": 1},
+            progress_bar=True,
+        )["samples"][2**7 :, -1, 0]
+
+        samples_sc = sample(*setup(problem, 42, 1), 2**7)[1][0, :, 0]
+
+        self.assertLess(scipy.stats.wasserstein_distance(samples_sc, samples_nrpt), 0.1)
