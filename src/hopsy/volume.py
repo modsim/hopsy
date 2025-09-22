3f31f47880d17c24ef37739cc35fbe375537bd0c
Jadebeck, Johann Fredrik <j.jadebeck@fz-juelich.de>
2025-09-22 14:14:52 +0200

    pre-commit fixes

--- src/hopsy/volume.py
+++ src/hopsy/volume.py
@@ -23,10 +23,10 @@ _s = _submodules
 
 
 def get_first_variance(
-       n_dims: int, radius: float, tolerance: float, safety_factor: float = 0.5
+    n_dims: int, radius: float, tolerance: float, safety_factor: float = 0.5
 ) -> float:
     """Cooling Gaussians, see DOI 10.1007/s12532-015-0097-z"""
-    variance = safety_factor * radius / _s.chi2.ppf(1-tolerance, n_dims)
+    variance = safety_factor * radius / _s.chi2.ppf(1 - tolerance, n_dims)
     return variance
 
 
@@ -36,7 +36,7 @@ def get_next_variance(variance: float, n_dim: int) -> float:
 
 
 def sample_gaussian(
-        problem: _c.Problem, center, variance, random_seed, n_samples, n_procs
+    problem: _c.Problem, center, variance, random_seed, n_samples, n_procs
 ):
     """Cooling Gaussians, see DOI 10.1007/s12532-015-0097-z"""
     gaussian_problem = _c.Problem(
@@ -54,27 +54,29 @@ def estimate_log_ratio(samples, center, variance_im1: float, variance_i: float):
     square_norm = ((samples - center) ** 2).sum(axis=-1)
     if variance_i is None:
         # this is for tail-correction by comparing gauss to uniform
-        log_weights = 0.5 * square_norm * (1. / variance_im1)
+        log_weights = 0.5 * square_norm * (1.0 / variance_im1)
     else:
-        log_weights = 0.5 * square_norm * (1. / variance_im1 - 1. / variance_i)
+        log_weights = 0.5 * square_norm * (1.0 / variance_im1 - 1.0 / variance_i)
     log_ratio = _s.logsumexp(log_weights.ravel()) - _s.numpy.log(log_weights.size)
-    log_squared_ratio = _s.logsumexp(2. * log_weights.ravel()) - _s.numpy.log(log_weights.size)
-    log_variance = _s.numpy.exp(log_squared_ratio - 2. * log_ratio) - 1.
+    log_squared_ratio = _s.logsumexp(2.0 * log_weights.ravel()) - _s.numpy.log(
+        log_weights.size
+    )
+    log_variance = _s.numpy.exp(log_squared_ratio - 2.0 * log_ratio) - 1.0
     log_weights = log_weights.reshape((*log_weights.shape, -1))
     effective_sample_size = ess(log_weights)[0][0]
-    mc_error = _s.numpy.sqrt(max(log_variance, 0.) / effective_sample_size)
+    mc_error = _s.numpy.sqrt(max(log_variance, 0.0) / effective_sample_size)
     return log_ratio, mc_error
 
 
 def estimate_polytope_log_volume(
-        problem,
-        max_iterations=30,
-        tolerance=1e-2,
-        compute_rounding=True,
-        n_procs=1,
-        sample_batch_size=10000,
-        get_first_variance_fn=get_first_variance,
-        get_next_variance_fn=get_next_variance,
+    problem,
+    max_iterations=30,
+    tolerance=1e-2,
+    compute_rounding=True,
+    n_procs=1,
+    sample_batch_size=10000,
+    get_first_variance_fn=get_first_variance,
+    get_next_variance_fn=get_next_variance,
 ):
     """Cooling Gaussians, see DOI 10.1007/s12532-015-0097-z"""
     if compute_rounding:
@@ -84,7 +86,9 @@ def estimate_polytope_log_volume(
 
     n_dims = p.A.shape[1]
     center, radius = compute_chebyshev_center_and_radius(p)
-    variances = [get_first_variance_fn(n_dims=n_dims, radius=radius, tolerance=tolerance)]
+    variances = [
+        get_first_variance_fn(n_dims=n_dims, radius=radius, tolerance=tolerance)
+    ]
 
     log_ratios = []
     log_ratio_errors = []
@@ -118,15 +122,19 @@ def estimate_polytope_log_volume(
         n_samples=sample_batch_size,
         random_seed=random_seed,
     )
-    log_ratio, mc_error_log_ratio = estimate_log_ratio(samples, center, variance_im1=variances[-1], variance_i=None)
+    log_ratio, mc_error_log_ratio = estimate_log_ratio(
+        samples, center, variance_im1=variances[-1], variance_i=None
+    )
     log_ratios.append(log_ratio)
     log_ratio_errors.append(mc_error_log_ratio)
 
-
-    log_volume_estimate = _s.numpy.sum(_s.numpy.array(log_ratios)) + n_dims * 0.5 * _s.numpy.log(
-        2 * _s.numpy.pi * variances[0])
+    log_volume_estimate = _s.numpy.sum(
+        _s.numpy.array(log_ratios)
+    ) + n_dims * 0.5 * _s.numpy.log(2 * _s.numpy.pi * variances[0])
     if compute_rounding:
         rounding_factor = _s.numpy.linalg.slogdet(p.transformation).logabsdet
         log_volume_estimate += rounding_factor
-    log_volume_error = _s.numpy.sqrt(_s.numpy.sum(_s.numpy.square(_s.numpy.array(log_ratio_errors))))
+    log_volume_error = _s.numpy.sqrt(
+        _s.numpy.sum(_s.numpy.square(_s.numpy.array(log_ratio_errors)))
+    )
     return log_volume_estimate, log_volume_error
