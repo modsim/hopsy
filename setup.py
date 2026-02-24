9b787d902faead12370a0a5626265ef82abb4bb4
Johann Fredrik Jadebeck <j.jadebeck@fz-juelch.de>
2026-02-24 16:35:12 +0100

    reduce dependencies

--- setup.py
+++ setup.py
@@ -165,15 +165,11 @@ setup(
     install_requires=[
         "PolyRound>=0.2.10",
         "optlang>=1.7.0",
-        "arviz<=0.17.0; python_version<='3.9'",
-        "arviz; python_version>'3.9'",
+        "arviz",
         "numpy",
         "pandas",
         "tqdm",
-        "matplotlib",
-        "scikit-learn",
-        "scipy<1.13.0; python_version<='3.9'",
-        "scipy; python_version>'3.9'",
+        "scipy",
     ],
     zip_safe=False,
 )
