bf59f163c653d185c756b223f0d194eafe2a5034
Johann Fredrik Jadebeck <j.jadebeck@fz-juelch.de>
2026-02-26 12:31:34 +0100

    modernize packaging

--- src/hopsy/__init__.py
+++ src/hopsy/__init__.py
@@ -1,10 +1,9 @@
 from .core import *
 
 del globals()["MarkovChain"]
+from ._version import __version__ as __core_version__
 from .callback import *
-from .core import __build__ as __core_build__
 from .core import __is_debug__
-from .core import __version__ as __core_version__
 from .definitions import *
 from .examples import *
 from .lp import *
@@ -12,5 +11,4 @@ from .misc import *
 from .tuning import *
 from .volume import *
 
-__build__ = __core_build__.strip('"').strip("'")
 __version__ = __core_version__.strip('"').strip("'")
